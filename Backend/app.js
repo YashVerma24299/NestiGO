306e5bc52c4d323d1a2a9031a63cd37c83077da9
Yash <ritikverma23@gmail.com>
2025-05-27 21:13:37 +0530

    I have created the routes for the CRUD operations, and they are working properly.

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,9 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 
-
+//method-override
+const methodOverride = require('method-override');
+app.use(methodOverride("_method"));
 
 
 app.get('/', (req,res)=>{
@@ -53,9 +55,41 @@ app.get('/listings', async (req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 })
+//add new Route for specific listing 
+app.get('/listings/new',  (req,res)=>{
+    res.render("listings/new.ejs");
+})
+app.post('/listings', async (req,res)=>{
+    // let listing = req.body.listing;
+    // console.log(listing)
+    // await Listing.insertOne(listing);
+    // res.redirect("/listings");
+
+    const newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+});
 //show Route specific listing
 app.get('/listings/:id', async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
+})
+//edit route
+app.get('/listings/:id/edit', async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+//update route
+app.put('/listings/:id', async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+//delete route
+app.delete('/listings/:id', async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 })
