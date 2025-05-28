7188a37a0995e89ca0ef6b11211c1769f09eb569
Yash <ritikverma23@gmail.com>
2025-05-28 17:14:16 +0530

    Ensure the AllListings route is styled appropriately using CSS

--- Backend/init/data.js
+++ Backend/init/data.js
@@ -5,10 +5,7 @@ const sampleListings = [
     price: "120",
     location: "AshevilleNC",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1589911057171-44545e48c56b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1589911057171-44545e48c56b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Luxury Beachfront Villa",
@@ -16,10 +13,7 @@ const sampleListings = [
     price: "350",
     location: "MalibuCA",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url: "https://plus.unsplash.com/premium_photo-1675745329954-9639d3b74bbf?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1675745329954-9639d3b74bbf?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Modern Downtown Loft",
@@ -27,10 +21,7 @@ const sampleListings = [
     price: "2000",
     location: "NewYorkNY",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url:"https://plus.unsplash.com/premium_photo-1661923725782-f73c990fbddf?q=80&w=1129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1661923725782-f73c990fbddf?q=80&w=1129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Rustic Mountain Retreat",
@@ -38,10 +29,7 @@ const sampleListings = [
     price: "150",
     location: "DenverCO",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1455587734955-081b22074882?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1455587734955-081b22074882?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Charming Countryside Cottage",
@@ -49,10 +37,7 @@ const sampleListings = [
     price: "2308",
     location: "NashvilleTN",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url:"https://plus.unsplash.com/premium_photo-1675745329804-897876b0fbf0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1675745329804-897876b0fbf0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Sunny Beach House",
@@ -60,10 +45,7 @@ const sampleListings = [
     price: "1526",
     location: "MiamiFL",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Secluded Jungle Bungalow",
@@ -71,10 +53,7 @@ const sampleListings = [
     price: "5678",
     location: "Bali",
     country: "Indonesia",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Historic Paris Apartment",
@@ -82,10 +61,7 @@ const sampleListings = [
     price: "10890",
     location: "Paris",
     country: "France",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
 
@@ -94,10 +70,7 @@ const sampleListings = [
     price: "7830",
     location: "LakeTahoeCA",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Japanese Ryokan Experience",
@@ -105,10 +78,7 @@ const sampleListings = [
     price: "21002",
     location: "Kyoto",
     country: "Japan",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1535827841776-24afc1e255ac?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1535827841776-24afc1e255ac?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Ski-In Ski-Out Chalet",
@@ -116,10 +86,7 @@ const sampleListings = [
     price: "2291",
     location: "AspenCO",
     country: "USA",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Tropical Island Villa",
@@ -127,10 +94,7 @@ const sampleListings = [
     price: "19202",
     location: "Maldives",
     country: "Maldives",
-    image: {
-      filename: "listingimage",
-      url:"https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Classic London Flat",
@@ -138,10 +102,7 @@ const sampleListings = [
     price: "29992",
     location: "London",
     country: "UK",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1582719508461-905c673771fd?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1582719508461-905c673771fd?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Safari Tent Retreat",
@@ -149,10 +110,7 @@ const sampleListings = [
     price: "9999",
     location: "Serengeti",
     country: "Tanzania",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Greek Island Escape",
@@ -160,10 +118,7 @@ const sampleListings = [
     price: "89002",
     location: "Santorini",
     country: "Greece",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1545175707-9eec1209f720?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1545175707-9eec1209f720?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Forest Treehouse",
@@ -171,10 +126,7 @@ const sampleListings = [
     price: "2911",
     location: "British Columbia",
     country: "Canada",
-    image: {
-      filename: "listingimage",
-      url: "https://plus.unsplash.com/premium_photo-1661340695541-ee1ca7efedd0?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1661340695541-ee1ca7efedd0?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Floating Houseboat",
@@ -182,10 +134,7 @@ const sampleListings = [
     price: "8291",
     location: "Amsterdam",
     country: "Netherlands",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1599722585837-c1cb8eea32ff?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1599722585837-c1cb8eea32ff?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Alpine Ski Lodge",
@@ -193,10 +142,7 @@ const sampleListings = [
     price: "1111",
     location: "Zermatt",
     country: "Switzerland",
-    image: {
-      filename: "listingimage",
-      url: "https://plus.unsplash.com/premium_photo-1672097247893-4f8660247b1f?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://plus.unsplash.com/premium_photo-1672097247893-4f8660247b1f?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Royal Heritage Hotel",
@@ -204,10 +150,7 @@ const sampleListings = [
     price: "6666",
     location: "Jaipur",
     country: "India",
-    image: {
-      filename: "listingimage",
-      url:"https://images.unsplash.com/photo-1546412414-e1885259563a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1546412414-e1885259563a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
 
@@ -216,10 +159,7 @@ const sampleListings = [
     price: "2920",
     location: "Lapland",
     country: "Finland",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1568229988520-4bc288da81f7?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
+    image: "https://images.unsplash.com/photo-1568229988520-4bc288da81f7?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
