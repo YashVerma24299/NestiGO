036b6e403702168469e77e9ec096da19b60e4c94
Yash <ritikverma23@gmail.com>
2025-05-28 18:29:15 +0530

    Find small error and corrected them.

--- Backend/models/listing.js
+++ Backend/models/listing.js
@@ -10,13 +10,10 @@ const listingSchema =new Schema({
         type: String
     },
     image: {
-        filename: String,
-        url: {
             type: String,
             default: "https://images.unsplash.com/photo-1568495248636-6432b97bd949?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww",
             set: (v) => v && v.trim() !== "" ? v : "https://images.unsplash.com/photo-1568495248636-6432b97bd949?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww"
-        }
-    },
+        },
     price: {
         type: Number,
         // required: true
