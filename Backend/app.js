c0e97668b1b8d8e6139d12ab30b9fed1474375bf
Yash <ritikverma23@gmail.com>
2025-05-31 19:07:22 +0530

    Restructure the server side files.

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
-const Listing = require('./models/listing.js')
+
+//for ejs
+const path = require('path');
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
+//method-override
+const methodOverride = require('method-override');
+app.use(methodOverride("_method"));
+
+//ejs-mate also prefer include/partial
+//it  used for repetetive code in ejs files
+//like header, footer, navbar etc.
+const ejsMate = require('ejs-mate');
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// used for error handling in async functions
+const ExpressError =require('./utils/ExpressError.js');
 
 //express
 app.listen(port, () =>{
@@ -24,146 +43,18 @@ main()
     .catch((err) =>{
         console.log(err);
     });
-
-//for ejs
-const path = require('path');
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
-
-//method-override
-const methodOverride = require('method-override');
-app.use(methodOverride("_method"));
-
-//ejs-mate also prefer include/partial
-//it  used for repetetive code in ejs files
-//like header, footer, navbar etc.
-const ejsMate = require('ejs-mate');
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//  used for error handling in async functions
-const wrapAsync =require('./utils/wrapAsync.js');
-const ExpressError =require('./utils/ExpressError.js');
-
-// listingSchema and reviewSchema
-const { listingSchema, reviewSchema}  = require('./schema.js');
-
-//Review Schema
-const Review = require('./models/review.js');
-
-
-
-
-
-
-
 app.get('/', (req,res)=>{
     res.send("Root")
 })
 
-const validateListing = (req, res, next) => {
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
-// app.get('/testListing', async (req,res) =>{
-//     let sample = new Listing({
-//         title: 'My new Villa',
-//         description: 'A beautiful villa with a sea view',
-//         price: 1200,
-//         location: 'Panaji, Goa',
-//         country: 'India'
-//     })
-//     await sample.save();
-//     console.log("Sample listing saved");
-//     res.send("Successfull testing");
-// })
-app.get('/listings', wrapAsync(async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
-//add new Route for specific listing 
-app.get('/listings/new',  (req,res)=>{
-    res.render("listings/new.ejs");
-})
-app.post('/listings', validateListing , wrapAsync(async (req,res, next)=>{
-    // let listing = req.body.listing;
-    // console.log(listing)
-    // await Listing.insertOne(listing);
-    // res.redirect("/listings");
+// For listing routes
+const listings = require('./routes/listing.js');
+app.use('/listings', listings);
 
-    const newlisting = new Listing(req.body.listing);
-    await newlisting.save();
-    res.redirect("/listings");
-}));
-
-//show Route specific listing
-app.get('/listings/:id',wrapAsync( async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate('reviews');
-    res.render("listings/show.ejs", {listing});
-}));
-//edit route
-app.get('/listings/:id/edit', wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}));
-//update route
-app.put('/listings/:id',validateListing, wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-//delete route
-app.delete('/listings/:id', wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
-
-
-
-// Reviews
-const validateReview = (req, res, next) => {
-    let {error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
-app.post('/listings/:id/reviews', validateReview, wrapAsync(
-    async (req,res) =>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    // console.log("Review added successfully");
-    // res.send("Review added successfully");
-    res.redirect(`/listings/${listing._id}`);
-}));
-//delete
-app.delete('/listings/:id/reviews/:reviewId', wrapAsync(async (req,res)=>{
-    let {id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findById(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
-
-
-
+// For review routes
+const reviews = require('./routes/review.js');
+app.use('/listings/:id/reviews', reviews);
 
 
 //error handling
