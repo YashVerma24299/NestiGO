42c1edb5bd3e1f68de6b467253fc4e6e8358fbcd
Yash <ritikverma23@gmail.com>
2025-06-03 19:34:48 +0530

    Authorization for listings

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing.js");
+const ExpressError =require('./utils/ExpressError.js');
+const { listingSchema}  = require('./schema.js');
+const { reviewSchema}  = require('./schema.js');
+
 module.exports.isLoggedIn = (req,res,next) =>{
     // if(!req.isAuthenticated()){
     //     // If user is not registerd
@@ -28,4 +33,34 @@ module.exports.savedRedirectUrl = (req,res, next) =>{
         res.locals.redirectUrl =req.session.redirectUrl;
     }
     next();
+}
+
+module.exports.isOwner =async (req,res,next) =>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+        req.flash("error", "You don't have permisssion to edit");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next) => {
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+module.exports.validateReview = (req, res, next) => {
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
 }
--- Backend/routes/listing.js
+++ Backend/routes/listing.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const wrapAsync =require('../utils/wrapAsync.js');
-const ExpressError =require('../utils/ExpressError.js');
-const { listingSchema}  = require('../schema.js');
 const Listing = require('../models/listing.js')
-const {isLoggedIn} =require("../middleware.js");
+const {isLoggedIn, isOwner, validateListing} =require("../middleware.js");
 
-
-const validateListing = (req, res, next) => {
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
 //Index Route
 router.get('/', wrapAsync(async (req,res)=>{
@@ -64,7 +52,7 @@ router.post('/', validateListing , wrapAsync(async (req,res, next)=>{
 }));
 
 //edit route
-router.get('/:id/edit',isLoggedIn, wrapAsync(async (req,res)=>{
+router.get('/:id/edit',isLoggedIn, isOwner, wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -75,7 +63,7 @@ router.get('/:id/edit',isLoggedIn, wrapAsync(async (req,res)=>{
 }));
 
 //update route
-router.put('/:id',isLoggedIn,validateListing, wrapAsync(async (req,res)=>{
+router.put('/:id',isLoggedIn, isOwner, validateListing, wrapAsync(async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     req.flash("success", "Listing Updated!")
@@ -83,7 +71,7 @@ router.put('/:id',isLoggedIn,validateListing, wrapAsync(async (req,res)=>{
 }));
 
 //delete route
-router.delete('/:id',isLoggedIn, wrapAsync(async (req,res)=>{
+router.delete('/:id',isLoggedIn, isOwner, wrapAsync(async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing Deleted!")
